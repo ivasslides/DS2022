9e65a54f7ec0e4733e6206bcb138c8be0f46127f
ivasslides <ivasslides@chonkster>
2024-09-22 15:42:48 -0400

    part 3

---
+++ my-work/lab3/github-events.py
@@ -0,0 +1,17 @@
+import os 
+import json
+import requests 
+
+
+GHUSER = os.getenv('GITHUB_USER')
+print(GHUSER)
+
+url = 'https://api.github.com/users/ivasslides/events'
+
+r = json.loads(requests.get(url).text)
+
+for x in r[:5]:
+    event = x['type'] + ' :: ' + x['repo']['name']
+    print(event)
+
+print(r)
