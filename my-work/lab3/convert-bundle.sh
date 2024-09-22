d2c93f169d63d1398372b64a2bef106c59e7fcc7
ivasslides <ivasslides@chonkster>
2024-09-22 15:16:00 -0400

    part 2 completed

--- my-work/lab3/convert-bundle.sh
+++ my-work/lab3/convert-bundle.sh
@@ -4,7 +4,7 @@ wget https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
 tar -xzf lab3-bundle.tar.gz
 
 #tr can remove spaces
-cat myfile.tsv | tr -s '\n' > mynewfile.tsv
+cat lab3_data.tsv | tr -s '\n' > mynewfile.tsv
 
 #use `tr` - translate
 tr '\t' ',' < mynewfile.tsv > file.csv
@@ -14,5 +14,5 @@ WC=$(($(wc -l < file.csv) - 1))
 echo $WC
 
 #create new tarball 
-tar -czf converted-archive.tar.gz file.csv
+tar -czf converted-archive.tar.gz file.csv 
 
