d1c0fe24ff1f1ef4f00cf690f39dff8a0a10f7cc
ivasslides <ivasslides@chonkster>
2024-09-22 15:06:30 -0400

    pt 2 not done yet

---
+++ my-work/lab3/convert-bundle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+wget https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
+tar -xzf lab3-bundle.tar.gz
+
+#tr can remove spaces
+cat myfile.tsv | tr -s '\n' > mynewfile.tsv
+
+#use `tr` - translate
+tr '\t' ',' < mynewfile.tsv > file.csv
+
+#word count
+WC=$(($(wc -l < file.csv) - 1)) 
+echo $WC
+
+#create new tarball 
+tar -czf converted-archive.tar.gz file.csv
+
